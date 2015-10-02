beed71478f83d300c967c0582b8d21f2051947dd
airstruck <airstruck>
2015-10-01 22:07:54 -0400

    remove whitespace at EOL

--- knife/base.lua
+++ knife/base.lua
@@ -1,13 +1,12 @@
 return {
     extend = function (self, subtype)
         return setmetatable(subtype or {}, {
-            __index = self, 
+            __index = self,
             __call = function (self, ...)
-                local instance = setmetatable({}, { __index = self }) 
+                local instance = setmetatable({}, { __index = self })
                 return instance, instance:constructor(...)
-            end 
+            end
         })
     end,
     constructor = function () end,
 }
-
