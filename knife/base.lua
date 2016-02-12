22de9d01cfd3decb30d0ca6bce2370ecd5ab9a23
airstruck <airstruck>
2016-02-12 10:44:06 -0500

    knife.base: Reuse instance metatable

--- knife/base.lua
+++ knife/base.lua
@@ -1,12 +1,15 @@
 return {
     extend = function (self, subtype)
-        return setmetatable(subtype or {}, {
+        subtype = subtype or {}
+        local meta = { __index = subtype }
+        return setmetatable(subtype, {
             __index = self,
             __call = function (self, ...)
-                local instance = setmetatable({}, { __index = self })
+                local instance = setmetatable({}, meta)
                 return instance, instance:constructor(...)
             end
         })
     end,
     constructor = function () end,
 }
+
