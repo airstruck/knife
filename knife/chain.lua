366789e4d6ae19cf9b05950cd3995f21b59d363e
airstruck <airstruck>
2016-04-02 22:59:08 -0400

    minor chain refactor

--- knife/chain.lua
+++ knife/chain.lua
@@ -1,23 +1,23 @@
+local function Invoker (links, index)
+    return function (...)
+        local link = links[index]
+        if not link then
+            return
+        end
+        local continue = Invoker(links, index + 1)
+        local returned = link(continue, ...)
+        if returned then
+            returned(function (_, ...) continue(...) end)
+        end
+    end
+end
+    
 return function (...)
     local links = { ... }
 
-    local function Invoker (index)
-        return function (...)
-            local link = links[index]
-            if not link then
-                return
-            end
-            local continue = Invoker(index + 1)
-            local returned = link(continue, ...)
-            if returned then
-                returned(function (_, ...) continue(...) end)
-            end
-        end
-    end
-
     local function chain (...)
         if not (...) then
-            return Invoker(1)(select(2, ...))
+            return Invoker(links, 1)(select(2, ...))
         end
         local offset = #links
         for index = 1, select('#', ...) do
